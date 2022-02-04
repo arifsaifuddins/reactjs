4a218e369c694cbbe0b6b06f051da4eb7a6893a4
saifuddien <ariefsaifuddien01@gmail.com>
2022-02-05 00:42:49 +0200

    template

---
+++ template.js
@@ -0,0 +1,11 @@
+const arief = /*template*/ ` 
+  <h1>satu</h1>
+  <style>
+    h1 {
+      color: red;
+    }
+  </style>
+`;
+
+console.log(arief);
+document.querySelector('.arief').innerHTML = arief;
